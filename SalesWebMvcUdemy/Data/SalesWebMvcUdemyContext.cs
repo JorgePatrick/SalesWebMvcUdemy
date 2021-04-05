1c831585477847d7bb3ca9bb09e45ec4d571e353
Jorge Patrick <jorgepatrickdev@gmail.com>
2021-04-05 18:59:02 -0300

    CRUD Scaffold - Department

---
+++ SalesWebMvcUdemy/Data/SalesWebMvcUdemyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcUdemy.Models;
+
+namespace SalesWebMvcUdemy.Data
+{
+    public class SalesWebMvcUdemyContext : DbContext
+    {
+        public SalesWebMvcUdemyContext (DbContextOptions<SalesWebMvcUdemyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcUdemy.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebMvcUdemy/Startup.cs
+++ SalesWebMvcUdemy/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcUdemy.Data;
 
 namespace SalesWebMvcUdemy
 {
@@ -33,6 +35,9 @@ namespace SalesWebMvcUdemy
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcUdemyContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcUdemyContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
