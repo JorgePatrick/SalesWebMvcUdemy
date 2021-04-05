29c519a9e7a759230a6cc119aac0b668d25faab3
Jorge Patrick <jorgepatrickdev@gmail.com>
2021-04-05 20:01:04 -0300

    MySQL adaptation and first migration

--- SalesWebMvcUdemy/Startup.cs
+++ SalesWebMvcUdemy/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMvcUdemy
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcUdemyContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcUdemyContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcUdemyContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvcUdemy")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
